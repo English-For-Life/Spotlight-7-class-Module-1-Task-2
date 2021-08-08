f5cb2d23389681ee2d56a931aa354b7553728e98
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-08 22:30:45 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,77 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"John leads an ...... lifestyle.",
+        options:["unhealthy", "unwelcome"],
+        answer: 0
+    },
+    {
+        q:"We really like the ...... and quiet in this village.",
+        options:["peace", "landscape"],
+        answer: 1
+    },
+    {
+        q:"There is a low rate of ...... in big cities.",
+        options:["unemployment", "living"],
+        answer: 0
+    },
+    {
+        q:"It's a crime to ...... into a house.",
+        options:["look", "break"],
+        answer: 1
+    },
+    {
+        q:"I miss the hustle and ...... of LA.",
+        options:["bustle", "noise"],
+        answer: 0
+    },
+    {
+        q:"I love ...... with my friends.",
+        options:["saying", "chatting"],
+        answer: 1
+    },
+    {
+        q:"Public transport is ...... in my town.",
+        options:["convenient", "heavy"],
+        answer: 0
+    },
+    {
+        q:"You are ...... when you miss home.",
+        options:["hometown", "homesick"],
+        answer: 1
+    },
+    {
+        q:"He's ...... outside lights in this garden.",
+        options:["installing", "inventing"],
+        answer: 0
+    },
+    {
+        q:"I hate the ...... noise in Paris.",
+        options:["crowded", "constant"],
+        answer: 1
+    },
+    {
+        q:"He feels ...... away from his family.",
+        options:["lonely", "friendly"],
+        answer: 0
+    },
+    {
+        q:"Let's ...... a DVD tonight.",
+        options:["check", "rent"],
+        answer: 1
+    },
+    {
+        q:"On a hot day we ...... beach volleyball.",
+        options:["play", "do"],
+        answer: 0
+    },
+    {
+        q:"Everything is ...... at hand in big cities.",
+        options:["easy", "close"],
+        answer: 1
+    }
+
+
+
+]
